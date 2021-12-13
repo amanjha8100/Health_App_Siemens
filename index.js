916f9554dbf3c997d3fcf3026caebaa096da731e
Aman Prakash Jha <amanjha7151@gmail.com>
2021-12-14 00:55:17 +0530

    revamped to mongoDB

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ app.use(express.static(path.join(__dirname, 'vendor')));
 app.use(routes);
 
 
-app.use((err, req, res, next) => {
-    // console.log(err);
-    return res.send('Internal Server Error');
-});
+// app.use((err, req, res, next) => {
+//     console.log(err);
+//     return res.send('Internal Server Error');
+// });
 
 app.listen(3000, () => console.log('Server is runngin on port 3000'));
